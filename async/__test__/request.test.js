5580664d312c8170202c848c007b0fcc3403f743
Ian Khor <iankhorbc.thecoder@gmail.com>
2019-06-06 19:47:55 +1000

    add GOOD and BAD examples of unhandled promise rejections example

--- async/__test__/request.test.js
+++ async/__test__/request.test.js
@@ -1,6 +1,15 @@
 import request from './../request'
 jest.mock('./../request')
 
+const asyncFailedRequest = jest.fn(() => {
+  return Promise.reject('boom');
+})
+
+const asyncSuccessRequest = jest.fn(() => {
+  return Promise.resolve('yay');
+
+})
+
 describe('request', () => {
   describe('mocking', () => {
     test('is not using real request', async () => {
@@ -15,6 +24,29 @@ describe('request', () => {
       const response = await request('abc');
 
       expect(response).toBe('mocked request: abc')
+    })
+
+    describe('Promises', () => {
+      describe('Rejected', () => {
+        test('for false positives', () => {
+          // this triggers an unhandle promise reject, with test passing as a false positive
+          expect(asyncFailedRequest()).resolves.toEqual({})
+          expect(asyncFailedRequest()).resolves.toEqual(undefined)
+        })
+
+        test('rejects with error message', () => {
+          // returns promise and assert promise rejections, with test passing correctly
+          return expect(asyncFailedRequest()).rejects.toEqual('boom')
+        })
+
+      })
+
+      describe('Resolves', () => {
+        test('success', async () => {
+          expect(await asyncSuccessRequest()).toEqual('yay')
+          expect(asyncSuccessRequest()).resolves.toEqual('yay')
+        })
+      })
     })
   })
 })
