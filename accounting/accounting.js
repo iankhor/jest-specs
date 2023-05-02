9e84779c92de4321a93733a6e9f5a0d674a7c6d5
Ian Khor <ian@Ians-Mac-mini.local>
2023-05-03 00:12:45 +1000

    experiment for double entry account

---
+++ accounting/accounting.js
@@ -0,0 +1,130 @@
+const {AccountsRepository, JournalEventsRepository} = require('./respositories')
+const {sumBy} = require('lodash')
+
+class AccountingService {
+    balance(accountId, date = "2099-12-31") {
+        const account = AccountsRepository.findById(accountId)
+        const events = JournalEventsRepository.findAllByAccountId(accountId, "asc", date)
+
+        const debits = events.filter(e => e.action === 'debit')
+        const credits = events.filter(e => e.action === 'credit')
+
+        const calculateDebitAccounts = () => sumBy(debits, 'amount') - sumBy(credits, 'amount')
+        const calculateCreditAccounts = () => sumBy(credits, 'amount') - sumBy(debits, 'amount')
+        
+        const accountType = {
+            "ASSET_RECEIVABLE": calculateDebitAccounts,
+            "ASSET_CASH": calculateDebitAccounts,
+            "EXPENSE": calculateDebitAccounts,
+            "LIABILITY":  calculateCreditAccounts
+        }
+
+        return accountType[account.type]()
+    }
+}
+
+class DebtorService {
+    charge(amount, accountId) {
+        const linkedAccountIds = AccountsRepository.findLinkedById(accountId).linkedAccountIds
+        const linkedAccounts = AccountsRepository.findAllByIds(linkedAccountIds)
+
+        const receivableAccount = linkedAccounts.find(a => a.type === "ASSET_RECEIVABLE")
+        const expenseTaxAccount = linkedAccounts.find(a => a.type === "EXPENSE")
+        const salesAccount = linkedAccounts.find(a => a.type === "LIABILITY")
+
+        const taxRate = 0.1
+        const amountTaxExclusive = amount / (1 + taxRate)
+        const taxAmount = amount - amountTaxExclusive
+
+        JournalEventsRepository.create([
+            {
+                "date": "2023-05-01",
+                "eventType": "Charges",
+                "accountId": receivableAccount.id,
+                "amount": amountTaxExclusive,
+                "action": "debit"
+              },
+              {
+                "date": "2023-05-01",
+                "eventType": "Charges",
+                "accountId": expenseTaxAccount.id,
+                "amount": taxAmount,
+                "action": "debit"
+              },
+              {
+                "date": "2023-05-01",
+                "eventType": "Charges",
+                "accountId": salesAccount.id,
+                "amount": amount,
+                "action": "credit"
+              }
+        ])
+    }
+}
+
+class CashService {
+    receipt(amount, accountId) {
+        const linkedAccountIds = AccountsRepository.findLinkedById(accountId).linkedAccountIds
+        const linkedAccounts = AccountsRepository.findAllByIds(linkedAccountIds)
+
+        const receivableAccount = linkedAccounts.find(a => a.type === "ASSET_RECEIVABLE")
+        const cashAccount = linkedAccounts.find(a => a.type === "ASSET_CASH")
+        const expenseTaxAccount = linkedAccounts.find(a => a.type === "EXPENSE")
+        const salesAccount = linkedAccounts.find(a => a.type === "LIABILITY")
+
+        const taxRate = 0.1
+        const amountTaxExclusive = amount / (1 + taxRate)
+        const taxAmount = amount - amountTaxExclusive
+
+        JournalEventsRepository.create([
+            {
+                "date": "2023-05-31",
+                "eventType": "CashReceipt",
+                "accountId": cashAccount.id, 
+                "amount": amount,
+                "action": "debit"
+            },
+            {
+                "date": "2023-05-31",
+                "eventType": "CashReceipt",
+                "accountId": receivableAccount.id,
+                "amount": amountTaxExclusive,
+                "action": "credit"
+            },
+            {
+                "date": "2023-05-31",
+                "eventType": "CashReceipt",
+                "accountId": expenseTaxAccount.id,
+                "amount": taxAmount,
+                "action": "credit"
+            },
+            {
+                "date": "2023-05-31",
+                "eventType": "CashReceipt",
+                "accountId": salesAccount.id,
+                "amount": amount,
+                "action": "debit"
+            }
+        ])
+    }
+    
+}
+
+const debtor = new DebtorService()
+const cash = new CashService()
+const accounting = new AccountingService()
+
+const chargeAmount = 1000
+const cashReceived = 500
+
+console.log(`charge ${chargeAmount}`)
+debtor.charge(chargeAmount, "foobar-123")
+console.log('debtor account balance excluding tax', accounting.balance("foobar-123"))
+console.log('debtor account balance tax component', accounting.balance("foobar-456"))
+
+console.log("\n")
+
+console.log(`receive cash  ${cashReceived}`)
+cash.receipt(cashReceived, "foobar-123")
+console.log('account balance excluding tax', accounting.balance("foobar-123"))
+console.log('debtor account balance tax component', accounting.balance("foobar-456"))
---
+++ accounting/respositories.js
@@ -0,0 +1,81 @@
+const accounts = [
+    {
+        "id": "foobar-123",
+        "name": "Debtor A Charges",
+        "type": "ASSET_RECEIVABLE"
+    },
+    {
+        "id": "foobar-456",
+        "name": "Debtor A Charges - Tax",
+        "type": "EXPENSE"
+    },
+    {
+        "id": "foobar-859",
+        "name": "Debtor A Unearned Revenue (Sales)",
+        "type": "LIABILITY"
+    },
+    {
+        "id": "foobar-999",
+        "name": "Bank Account",
+        "type": "ASSET_CASH"
+    }
+  ]
+
+const linkedAccounts = [
+    {
+        id: "foobar-123",
+        linkedAccountIds: ["foobar-123", "foobar-456", "foobar-859", "foobar-999"]
+    }
+]
+
+const journalsEvents = []
+
+function sortAscending(a, b) {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateA - dateB;
+}
+
+function sortDescending(a, b) {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateB - dateA;
+}
+
+class AccountsRepository {
+    static findById(id) {
+        return accounts.find(a => a.id === id)
+    }
+
+    static findAllByIds(ids) {
+        return accounts.filter(a => ids.includes(a.id))
+    }
+
+    static findLinkedById(id) {
+        return linkedAccounts.find(a => a.id === id)
+    }
+}
+
+
+class JournalEventsRepository {
+    static create(events = []) {
+        journalsEvents.push(...events)
+    }
+
+    static findByType(type) {
+        return journalsEvents.find(e => e.eventType === type)
+    }
+
+    static findAllByAccountId(id, order = 'asc', at = "2099-12-31") {
+        return journalsEvents
+                .filter(e => e.accountId === id)
+                .sort(order === 'asc' ? sortAscending : sortDescending)
+                .filter(e => {
+                    const date = new Date(e.date)
+                    const cutoff = new Date(at)
+                    return date <= cutoff
+                })
+    }
+}
+
+module.exports = {AccountsRepository, JournalEventsRepository}
