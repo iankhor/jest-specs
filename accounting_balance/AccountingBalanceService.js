d2c7b7747980051beae293a54a61d36f5800fe94
Ian Khor <ian@Ians-Mac-mini.local>
2023-06-04 11:04:48 +1000

    complete test

--- accounting_balance/AccountingBalanceService.js
+++ accounting_balance/AccountingBalanceService.js
@@ -1,17 +1,68 @@
-class JournalEntryRepository {
+import { isBefore, isEqual } from "date-fns"
+import {sumBy} from 'lodash'
 
+class JournalEntryRepository {
+    constructor(je) {
+        this.je = je
+    }
+
+    findCreatedAtTillDate(date) {
+        return this.je.filter(je => {
+            return isBefore(new Date(je.createdAt), new Date(date)) || isEqual(new Date(je.createdAt), new Date(date))
+        })
+    }
+
+    findByDocument(id) {
+        return this.je.find(je => je.document === id)
+    }
 }
 
+function sumByDebit(journalLines) {
+    return journalLines.entryType === "DR" ? journalLines.amount : 0
+}
+
+function sumByCredit(journalLines) {
+    return journalLines.entryType === "CR" ? journalLines.amount : 0
+}
+
+function calculateBalance(journalLines) {
+    const debits = sumBy(journalLines, sumByDebit)
+    const credits = sumBy(journalLines, sumByCredit)
+    const outstanding = debits - credits
+
+    return {
+        entry: outstanding >= 0 ? "DR" : "CR",
+        amount: outstanding
+    }
+}
 
 class AccountingBalanceService {
     constructor(jeRepository = new JournalEntryRepository()) {
         this.jeRepository = jeRepository
     }
 
-    debtor_account() {
-        return 2
+    debtor_account(accountId, date) {
+        const journalEntries = this.jeRepository.findCreatedAtTillDate(date)
+        const journalLines = journalEntries
+                                .flatMap(je => je.lines)
+                                .filter(jl => jl.accountId === accountId)
+
+        return calculateBalance(journalLines)
+    }
+
+    invoice(id, date) {
+        const invoiceJournalEntry = this.jeRepository.findByDocument(id)
+        const journalId = invoiceJournalEntry.id
+
+        const journalEntries = this.jeRepository.findCreatedAtTillDate(date)
+        const journalLines = journalEntries
+                                .flatMap(je => je.lines)
+                                .filter(jl => jl.referenceJournalId === journalId)
+
+        return calculateBalance(journalLines)
     }
 
 }
 
 export default AccountingBalanceService
+export { JournalEntryRepository }
--- accounting_balance/accounting_balance.test.js
+++ accounting_balance/accounting_balance.test.js
@@ -1,11 +1,72 @@
-import AccountingBalanceService from "./AccountingBalanceService"
+import AccountingBalanceService, {JournalEntryRepository} from "./AccountingBalanceService"
+import je from './journal_entries.json'
 
 describe("AccountingBalanceService", () =>{
-    it("debtor_account()", () =>{
-        const accountingBalance = new AccountingBalanceService()
-        expect(accountingBalance.debtor_account("debtor-1")).toEqual({
-            entry: "DR",
-            amount: "2500"
+    function subject(journalEntries) {
+        const jeRepository = new JournalEntryRepository(journalEntries)
+        const accountingBalanceService = new AccountingBalanceService(jeRepository)
+
+        return {accountingBalanceService}
+    }
+
+    describe("debtor_account()", () =>{
+        it("given debtor-1, return balance as at 2023-05-01", () =>{
+            const {accountingBalanceService}  = subject(je)
+            expect(accountingBalanceService.debtor_account("debtor-1", "2023-05-01")).toEqual({
+                entry: "DR",
+                amount: 1000
+            })
+        })
+
+
+        it("given debtor-1, return balance as at 2023-05-02", () =>{
+            const {accountingBalanceService}  = subject(je)
+            expect(accountingBalanceService.debtor_account("debtor-1", "2023-05-02")).toEqual({
+                entry: "DR",
+                amount: 3000
+            })
+        })
+
+        it("given debtor-1, return balance as at 2023-05-03", () =>{
+            const {accountingBalanceService}  = subject(je)
+            expect(accountingBalanceService.debtor_account("debtor-1", "2023-05-03")).toEqual({
+                entry: "DR",
+                amount: 2500
+            })
+        })
+
+        it("given debtor-2, return balance as at 2023-05-01", () =>{
+            const {accountingBalanceService}  = subject(je)
+            expect(accountingBalanceService.debtor_account("debtor-2", "2023-05-31")).toEqual({
+                entry: "DR",
+                amount: 0
+            })
+        })
+    })
+
+    describe("invoice()", () =>{
+        it("given invooice-1, return invoice balance as at 2023-05-01", () =>{
+            const {accountingBalanceService}  = subject(je)
+            expect(accountingBalanceService.invoice("invoice-1", "2023-05-01")).toEqual({
+                entry: "DR",
+                amount: 1000
+            })
+        })
+
+        it("given invooice-1, return invoice balance as at 2023-05-03", () =>{
+            const {accountingBalanceService}  = subject(je)
+            expect(accountingBalanceService.invoice("invoice-1", "2023-05-03")).toEqual({
+                entry: "DR",
+                amount: 500
+            })
+        })
+
+        it("given invooice-2, return invoice balance as at 2023-05-03", () =>{
+            const {accountingBalanceService}  = subject(je)
+            expect(accountingBalanceService.invoice("invoice-2", "2023-05-03")).toEqual({
+                entry: "DR",
+                amount: 2000
+            })
         })
     })
 })
